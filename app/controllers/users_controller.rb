7e073765b8eaffc4d321b3f1c2b4e3c659141128
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-02-04 23:56:53 +0100

    devise form psw update + ux improvement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,6 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
-  def update
-    @user = current_user
-    @user.name = username_params[:name]
-    if password_params[:password] != "" && password_params[:password] == password_params[:password_confirmation]
-      @user.password = password_params[:password_confirmation]
-    end
-    @user.save
-    redirect_to user_path(current_user)
-  end
-
   def destroy
     @user = current_user
     @user.destroy
