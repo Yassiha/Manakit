cd6b43efe3f961a1d1c4e4a3bbb2cda89c68b960
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-02-16 18:28:47 +0100

    add user to project

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   resources :projects do
+    resources :add_project_member, only: %w[new create destroy]
     resources :missions, only: %w[show new create edit update destroy]
   end
   resources :users, only: %w[show update destroy]
