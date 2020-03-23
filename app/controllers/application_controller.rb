3ced1362fb795398ae8cc65f03180e89a8863724
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-03-23 17:29:27 +0100

    front show project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,28 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  helper_method :status_color, :priority_color
+
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || projects_path
+  end
+
+  def status_color(item)
+    if item.status == 100
+      return "green"
+    else
+      return "gray"
+    end
+  end
+
+  def priority_color(item)
+    if item.priority == "Low"
+      return "green"
+    elsif item.priority == "Moderate"
+      return "orange"
+    elsif item.priority == "Critical"
+      return "red"
+    end
   end
 
 end
