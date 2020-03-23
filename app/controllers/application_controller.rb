a5302d1a823b09a8efe1f4a44490c7b480fc3d5a
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-03-23 19:18:11 +0100

    color

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  helper_method :status_color, :priority_color
+  helper_method :status_color, :priority_color, :date_color
 
 
   def after_sign_in_path_for(resource)
@@ -16,13 +16,23 @@ class ApplicationController < ActionController::Base
   end
 
   def priority_color(item)
-    if item.priority == "Low"
+    if item.priority == "Negligible"
       return "green"
     elsif item.priority == "Moderate"
       return "orange"
     elsif item.priority == "Critical"
       return "red"
     end
+  end
+
+  def date_color(item)
+    return "gray" if item.start_date > Time.now
+
+    return "green" if item.start_date >= Time.now && item.due_date < Time.now
+
+    return "orange" if item.start_date >= Time.now && item.due_date == Time.now
+
+    return "red" if item.start_date > Time.now && item.due_date > Time.now
   end
 
 end
