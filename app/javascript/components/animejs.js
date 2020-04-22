6d3bfefb5a8fe95df4bae818bf2ab0ad793e17ee
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-04-22 21:56:41 +0200

    homepage

--- app/javascript/components/animejs.js
+++ app/javascript/components/animejs.js
@@ -28,7 +28,7 @@ textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<
       opacity: 0,
       duration: 1000,
       easing: "easeOutExpo",
-      delay: 10000
+      delay: 6000
     });
   }
 
@@ -42,22 +42,22 @@ const anime1 = () => {
       opacity: [0,1],
       translateZ: 0,
       easing: "easeOutExpo",
-      duration: 700,
+      duration: 1000,
       delay: (el, i) => 70 * (i+1)
     }).add({
       targets: '.ml1 .line',
       scaleX: [0,1],
       opacity: [0.5,1],
       easing: "easeOutExpo",
-      duration: 800,
+      duration: 1200,
       offset: '-=875',
       delay: (el, i, l) => 80 * (l - i)
     }).add({
       targets: '.ml1',
       opacity: 0,
-      duration: 1200,
+      duration: 1400,
       easing: "easeOutExpo",
-      delay: 10000
+      delay: 6000
     });
 }
 
