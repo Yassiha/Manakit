ddc646757e8e27cfa6e7eb5d401d25428afd9995
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-04-23 19:19:06 +0200

    fail vanta

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,29 @@
 import "bootstrap";
 import { anime11 } from '../components/animejs';
 import { anime1 } from '../components/animejs';
-
+import React from 'react'
+import BIRDS from 'vanta/dist/vanta.birds.min'
 
 anime1();
 anime11();
+
+
+class MyComponent extends React.Component {
+  constructor() {
+    super()
+    this.vantaRef = React.createRef()
+  }
+  componentDidMount() {
+    this.vantaEffect = BIRDS({
+      el: this.vantaRef.current
+    })
+  }
+  componentWillUnmount() {
+    if (this.vantaEffect) this.vantaEffect.destroy()
+  }
+  render() {
+    return <div ref={this.vantaRef}>
+      Foreground content goes here
+    </div>
+  }
+}
