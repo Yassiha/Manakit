1674f218e61758be8f14383db27b7af3abbd86fd
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-05-02 02:47:46 +0200

    bug javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import { anime11 } from '../components/animejs';
 import { anime1 } from '../components/animejs';
 
-
+if (document.querySelector(".ml11 .letters")){
 anime1();
 anime11();
+}
