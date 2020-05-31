bfaec2eb73b75b54ec3a41b13875a8c49547fe2c
Yassine Iharkachen <y.iharkachen@gmail.com>
2020-05-31 03:54:23 +0200

    dashboard reforge

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,36 @@
 class DashboardController < ApplicationController
 
   def show
-    @projects = current_user.projects
-    @missions_finished = 0
-    @tasks_finished = 0
+    a = []
+    missions = []
+    projects = current_user.projects
+
+    projects.each do |project|
+      a << project
+    end
+
+
+    projects.each do |project|
+      project.missions.each do |mission|
+        a << mission
+        missions << mission
+      end
+    end
+
+    missions.each do |mission|
+      mission.tasks.each do |task|
+        a << task
+      end
+    end
+
+    missions.each do |mission|
+      mission.topics.each do |topic|
+        a << topic
+      end
+    end
+
+    @array = a.sort_by(&:updated_at)
+
   end
 
 end
